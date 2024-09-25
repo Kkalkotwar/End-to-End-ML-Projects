95ec1ceb3a6c3afd04f66e9190d70ab9bbf91ff4
KALKOTWAR <kunalkalkotwar21@gmail.com>
2024-09-26 00:54:08 +0530

    Exception File updated

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
 import sys
-import logging
+from src.logger import logging
+
 
 
 
@@ -24,3 +25,11 @@ class CustomeException(Exception):
     def __str__(self) :
         return self.error_message
     
+
+if __name__=="__main__":
+
+    try:
+        a = 1/0
+    except Exception as e:
+        logging.info("Cannot Divide by Zero")
+        raise CustomeException(e,sys)
